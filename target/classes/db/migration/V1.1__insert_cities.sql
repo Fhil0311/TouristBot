0b554a3adffe2b1e60c556ceb7fd7cf802f97174
Sergey Savitsky <sergeysavitsky22@gmail.com>
2020-04-07 20:08:19 +0300

    Edit migration

--- target/classes/db/migration/V1.1__insert_cities.sql
+++ target/classes/db/migration/V1.1__insert_cities.sql
@@ -1,1 +1,7 @@
-INSERT INTO `city`(`city_name`, `city_description`) VALUES('Minsk123', 'Minsk is beautiful');
+INSERT INTO `city`(`city_name`, `city_description`) VALUES('Minsk', 'World War II Museum');
+INSERT INTO `city`(`city_name`, `city_description`) VALUES('Moscow', 'the Red Square');
+INSERT INTO `city`(`city_name`, `city_description`) VALUES('Amsterdam', 'Van Gogh Museum');
+INSERT INTO `city`(`city_name`, `city_description`) VALUES('Kyiv', 'Independence Square');
+INSERT INTO `city`(`city_name`, `city_description`) VALUES('Berlin', 'Brandenburg Gate');
+INSERT INTO `city`(`city_name`, `city_description`) VALUES('Paris', 'The Eiffel Tower');
+INSERT INTO `city`(`city_name`, `city_description`) VALUES('New York', 'Times Square');
